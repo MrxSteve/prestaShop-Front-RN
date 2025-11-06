7fb146ac6bcc79e8bbf00abb8a6b6635e3cda6cf
ISMAEL ENRIQUE GARCIA HERNANDEZ <ismaelhernan1237@gmail.com>
2025-11-06 13:06:32 -0600

    creacion de types para la entidad roles

--- src/types/roles.ts
+++ src/types/roles.ts
@@ -3,42 +3,7 @@ export interface RolResponse {
   nombre: string;
 }
 
-export interface ListRolesQuery {
-  page: number;
-  size: number;
-  sort: string[];
+export interface RolRequest {
+    nombre: string;
 }
 
-export type ListRolesResponse = RolResponse[];
-
-export interface GetRolByIdParams {
-  id: number | string;
-}
-export type GetRolByIdResponse = RolResponse;
-
-export interface GetRolByNombreParams {
-  nombre: string;
-}
-export type GetRolByNombreResponse = RolResponse;
-
-export interface CreateRolRequest {
-  id?: number;
-  nombre: string;
-}
-export type CreateRolResponse = RolResponse;
-
-export interface UpdateRolParams {
-  id: number | string;
-}
-export interface UpdateRolRequest {
-  id?: number;
-  nombre: string;
-}
-export type UpdateRolResponse = RolResponse;
-
-export interface DeleteRolParams {
-  id: number | string;
-}
-export interface DeleteRolResponse {
-  message?: string;
-}
